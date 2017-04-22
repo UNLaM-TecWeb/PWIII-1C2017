8f9e84e693416c3753a74d30e841c133013d6261
Juanmanuel245 <juanmanuelcaffi@gmail.com>
2017-04-21 21:57:59 -0300

    Agregado el template, las carpetas para los css y js, las vistas (con layout), los controladores con sus metodos y la vista para administracion/peliculas

---
+++ Cine/Cine/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cine.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Cine/Cine/Controllers/PeliculasController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cine.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        //
+        // GET: /Peliculas/
+
+        public ActionResult Reservas()
+        {
+            return View();
+        }
+
+    }
+}
