d22b6c602689bd8763807527f959cd8009a709ef
German Rodrigo Alvarez <agro.075@gmail.com>
2017-05-01 21:02:18 -0300

    Generé la vista Reportes.cshtml y más cambios.
    - En el archivo globales.js cree una función nueva: DarFoco(...).
    - En el archivo _MasterLayout.cshtml corregí las referencias hechas con "@Url.Content("...")" por "@Url.Content('...')".
    - Agregué el archivo XHTML.pptx al directorio Documentación.

--- Cine/Cine/Content/Js/globales.js
+++ Cine/Cine/Content/Js/globales.js
@@ -2,3 +2,15 @@
     {
         $("#"+$div).show();
     }
+
+
+/**
+ * Le da el foco a un componente de la pagina a traves de su id. 
+ * 
+ * @param  {string} idComponente Es el id de algún componente válido capaz de recivir el foco. Por ejemplo: un campo de texto, una lista desplegable, etc.
+ * @return {none} La función no retorna valores.
+ * 
+ */
+function DarFoco(idComponente) {
+    document.getElementById(idComponente).focus();
+}
