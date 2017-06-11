250dd9c45e2b769b25128c7f65ba5c304d3d995c
Brian <briankuz.l@gmail.com>
2017-06-11 20:02:19 -0300

    Se realizo helpers de la vista Reserva y ClienteReserva

---
+++ Cine/DAL/PersistenciaReserva.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+   public  class PersistenciaReserva
+    {
+       MyContext ctx = new MyContext();
+       public List<Versiones> ObtenerVersiones()
+       {
+           var versiones = (from v in ctx.Versiones select v).ToList();
+           List<Versiones> listaversiones = new List<Versiones>();
+
+           foreach (Versiones version  in versiones)
+           {
+               Versiones ver =  new Versiones();
+               ver.IdVersion = version.IdVersion;
+               ver.Nombre = version.Nombre;
+               listaversiones.Add(ver);
+           }
+
+           return listaversiones;
+       }
+
+       public List<Reservas> ObtenerDias()
+       {
+           var dias = (from s in ctx.Reservas select s).ToList();
+           List<Reservas> listaversiones = new List<Reservas>();
+           foreach (Reservas reserva in dias)
+           {
+               Reservas re = new Reservas();
+               re.IdReserva = reserva.IdVersion;
+               re.FechaCarga = reserva.FechaCarga;
+               listaversiones.Add(re);
+           }
+           return listaversiones;
+       }
+
+       public List<Reservas> ObtenerHorarios()
+       {
+           var horarios = (from s in ctx.Reservas select s).ToList();
+           List<Reservas> listahorarios = new List<Reservas>();
+           foreach (Reservas reserva in horarios)
+           {
+               Reservas re = new Reservas();
+               re.IdReserva = reserva.IdVersion;
+               re.FechaHoraInicio = reserva.FechaHoraInicio;
+               listahorarios.Add(re);
+           }
+           return listahorarios;
+       }
+    }
+}
---
+++ Cine/Logica/ManejoReserva.cs
@@ -0,0 +1,36 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+   public  class ManejoReserva
+    {
+       PersistenciaReserva pReserva = new PersistenciaReserva();
+       PersistenciaSedes pSede = new PersistenciaSedes();
+
+
+       public List<Versiones> TraerVersiones()
+       {
+           return pReserva.ObtenerVersiones();
+       }
+
+       public List<Sedes> TraerSedes()
+       {
+           return pSede.ObtenerSedes();
+       }
+
+       public List<Reservas> TraerDias()
+       {
+           return pReserva.ObtenerDias();
+       }
+
+       public List<Reservas> TraerHorario()
+       {
+           return pReserva.ObtenerHorarios();
+       }
+    }
+}
