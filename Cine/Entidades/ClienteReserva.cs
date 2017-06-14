1b0e6a03740765d4c52ef4face51a65fda67e178
Brian <briankuz.l@gmail.com>
2017-06-14 00:34:59 -0300

    Se realizaron validacione de login

---
+++ Cine/Entidades/ClienteReserva.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+     [MetadataType(typeof(ClienteMetadata))]
+    public partial  class ClienteReserva
+    {
+         public class ClienteMetadata
+         {
+             [Required]
+             [StringLength(3, MinimumLength = 3)]
+             public string NombreUsuario { get; set; }
+         }
+
+    }
+}
---
+++ Cine/Entidades/Login.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Entidades
+{
+ [MetadataType(typeof(LoginMetadata))]
+    public partial class Login
+    {
+         public class LoginMetadata
+         {
+             [Required]
+             [StringLength(3, MinimumLength = 3)]
+             public string NombreUsuario { get; set; }
+         }
+    }
+}
