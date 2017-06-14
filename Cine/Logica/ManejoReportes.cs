6c156304738d3cb5d0418b6c0e3f125019f719c3
German Rodrigo Alvarez <agro.075@gmail.com>
2017-06-13 22:20:30 -0300

    Implementa la logica ManejoReportes.cs

---
+++ Cine/Logica/ManejoReportes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using Entidades;
+
+namespace Logica
+{
+    public class ManejoReportes
+    {
+        PersistenciaReserva pReserva = new PersistenciaReserva();
+
+        public List<Reservas> GenerarReporteReservas() // Traigo Todas las Reservas
+        {
+            return pReserva.ObtenerReservas();
+        }
+
+        public List<Reservas> GenerarReporteReservas(DateTime desde, DateTime hasta) // Traigo Todas las Reservas dentro de un intervalo de tiempo
+        {
+            return pReserva.ObtenerReservas(desde, hasta);
+        }
+
+    }
+}
